dca2fcb309a5b973713a9eff715c3f1fd6d52e93
Qing Miao <cdutwhu@outlook.com>
2020-06-16 22:31:50 +1000

    + jaegertracing

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	align "github.com/nsip/otf-classifier"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/labstack/echo-contrib/jaegertracing"
+	"github.com/labstack/echo/v4"
+	align "github.com/nsip/otf-classifier"
 )
 
 func main() {
+
+	os.Setenv("JAEGER_SERVICE_NAME", "OTF-CLASSIFIER")
+	os.Setenv("JAEGER_SAMPLER_TYPE", "const")
+	os.Setenv("JAEGER_SAMPLER_PARAM", "1")
+
 	align.Init()
 	e := echo.New()
+
+	// Add Jaeger Tracer into Middleware
+	c := jaegertracing.New(e, nil)
+	defer c.Close()
+
 	e.GET("/align", align.Align)
 	e.GET("/index", func(c echo.Context) error {
 		query := c.QueryParam("search")
 		ret, err := align.Search(query)
 		if err != nil {
 			return err
-		} else {
-			return c.String(http.StatusOK, string(ret))
 		}
+		return c.String(http.StatusOK, string(ret))
 	})
 	log.Println("Editor: localhost:1576")
 	e.Logger.Fatal(e.Start(":1576"))
--- go.mod
+++ go.mod
@@ -7,9 +7,13 @@ require (
 	github.com/blevesearch/bleve v1.0.9
 	github.com/glycerine/go-unsnap-stream v0.0.0-20190901134440-81cf024a9e0a // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/labstack/echo-contrib v0.9.0
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/nsip/curriculum-align v0.0.0-20190902015726-4a99208d6943
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tinylib/msgp v1.1.2 // indirect
+	github.com/uber/jaeger-client-go v2.24.0+incompatible // indirect
+	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
